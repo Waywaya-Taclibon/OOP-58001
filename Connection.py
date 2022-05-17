7124278c6995ef644b3095fc28a4ac8f5db0c9c7
Whywhyaaaa <94889709+Whywhyaaaa@users.noreply.github.com>
2022-05-17 18:50:33 +0800

    Delete Connection.py

--- Connection.py
+++
@@ -1,8 +0,0 @@
-import pyodbc
-
-try:
-    connect = pyodbc.connect(r'Driver= {Microsoft Access Driver (*.mdb, *.accdb)}; DBQ=C:\Users\Waywaya\Documents\Database1.accdb;')
-    print("Connected to a Database")
-
-except pyodbc.Error as e:
-    print("Error in Connection")
